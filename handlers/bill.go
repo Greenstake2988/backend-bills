ff883c20938e003a308e464508c0941c7750a70a
Oscar Daniel Vazquez Alcocer <oscar.va@valladolid.tecnm.mx>
2023-08-02 10:40:08 -0600

    Solucion Error Date en user -> bill

--- handlers/bill.go
+++ handlers/bill.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/mattn/go-sqlite3"
@@ -10,9 +11,10 @@ import (
 
 type Bill struct {
 	gorm.Model
-	UserID  uint    `json:"user_id"`
-	Concept string  `json:"concept"`
-	Price   float32 `json:"price"`
+	UserID  uint      `json:"user_id" binding:"required"`
+	Concept string    `json:"concept" binding:"required"`
+	Price   float32   `json:"price" binding:"required"`
+	Date    time.Time `json:"date" gorm:"type:date"`
 }
 
 // Rutas Bills
@@ -23,13 +25,13 @@ func (h *Handler) GetBillHandler(c *gin.Context) {
 
 	// Check if the "id" parameter is empty
 	if billID == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid bill ID"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID invalida"})
 		return
 	}
 
 	// find the first value in the data base with billID
 	if err := h.DB.First(&bill, billID).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Bill not found"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "Bill no encontrado"})
 		return
 	}
 
@@ -52,24 +54,29 @@ func (h *Handler) NewBillHandler(c *gin.Context) {
 
 	// Convierte el Json en el tipo de objeto que necesitamos
 	if err := c.ShouldBindJSON(&newBill); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
+	}
+
+	// Si la fecha no fue proporcionada o esta en blano le pones la fecha de Ahora
+	if newBill.Date.IsZero() {
+		newBill.Date = time.Now()
 	}
 
 	// Aqui guardaremos en la base de datos
 	if err := h.DB.Create(&newBill).Error; err != nil {
-		//Verificamos si se cumple alguna otra condicion
+		//Verificamos si cumple con la restricciond e llave foranea
 		if isForeignKeyConstraintError(err) {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Foreign key constraint failed"})
+			c.JSON(http.StatusBadRequest, gin.H{"error": "el user_id debe ser de un usuario real"})
 			return
 		}
 		// Si no es un error de clave externa, devolver otro mensaje de error genérico
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create Bill"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al crear la Bills"})
 		return
 	}
 
 	// Respond with a success message
-	c.JSON(http.StatusOK, gin.H{"message": "Received JSON", "data": newBill.Concept})
+	c.JSON(http.StatusOK, gin.H{"message": "JSON Recibido", "data": newBill.Concept})
 }
 func (h *Handler) DeleteBillHandler(c *gin.Context) {
 	var bill Bill
